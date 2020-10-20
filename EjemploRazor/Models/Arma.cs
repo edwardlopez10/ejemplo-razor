cb38be69bf58a5d78e0562d510087d2efb148de0
Edward Lopez <edward.lopez.ma@gmail.com>
2020-10-20 13:27:32 -0600

    Add project files.

---
+++ EjemploRazor/Models/Arma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjemploRazor.Models
+{
+    public class Arma
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Img { get; set; }
+    }
+}
---
+++ EjemploRazor/Pages/Armas.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EjemploRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EjemploRazor.Pages
+{
+    public class ArmasModel : PageModel
+    {
+        List<Arma> Armas
+        {
+            get
+            {
+                List<Arma> ListaFalsaDeArmas = new List<Arma>();
+                for (int i = 0; i < 10; i++)
+                {
+                    Arma ArmaFalsa = new Arma();
+                    ArmaFalsa.Id = i;
+                    ArmaFalsa.Nombre = "Arma " + i;
+                }
+                return ListaFalsaDeArmas;
+            }
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
